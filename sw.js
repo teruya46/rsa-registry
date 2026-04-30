3a01bdcc864a39237ca0865aa51ef9bea1dac32b
teruya46 <teruya31s+rsa@gmail.com>
2026-04-30 16:52:26 +0900

    Enable PWA install prompt with home-screen button

---
+++ sw.js
@@ -0,0 +1,11 @@
+self.addEventListener('install', () => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', () => {
+  // No-op. Required so Chrome treats the site as installable.
+});
